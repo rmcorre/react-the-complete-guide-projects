8178c5abf847af077fa25f139180dd125fb64325
Ross Correia <rmcorre@gmail.com>
2021-08-13 22:51:41 +0000

    Add the + and - button functionality to the CartItem component.

--- Section_11_Food_Order_App/src/store/cart-context.js
+++ Section_11_Food_Order_App/src/store/cart-context.js
@@ -3,6 +3,8 @@ import React from 'react';
 const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
+  //These methods are just placeholders for auto-completion
+  //They will be defined and added in CartProvider
   addItem: (item) => {},
   removeItem: (id) => {},
 });
